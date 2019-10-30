04c58284bb91246d852023949951a18cc6af753a
turuta7 <turuta51@gmail.com>
2019-10-30 22:54:58 +0200

    addText12:35

--- src/app.js
+++ src/app.js
@@ -1,33 +1,53 @@
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
-const https = require("https");
+const https = require('https');
 require('dotenv').config();
 
-let request = require('request');
+const request = require('request');
 
 const { token } = process.env;
 const bot = new TelegramBot(token, { polling: true });
-
-
 
 const app = express();
 const idTelegran = require('../models/').telegramt;
 
 app.get('/', (req, res) => {
-  res.send('server Work')
-})
+  res.send('server Work');
+});
+
+function text() {
+  return new Promise(resolve => {
+    request(
+      {
+        method: 'POST',
+        uri:
+          'https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&jsonp=parseQuote',
+      },
+      (error, response, body) => {
+        if (response.statusCode === 201) {
+          console.log('document saved as: http://mikeal.iriscouch.com/testjs/');
+        } else {
+          //  console.log(`status: ${response.statusCode}`);
+          const res = body.split('(')[1].split('"')[3];
+          resolve(res);
+        }
+      },
+    );
+  });
+}
 
 let time;
 let id = [];
 
-
 async function testUserDB() {
   const now = new Date();
-  time = `${now.getHours() + 3}:${now.getMinutes()}`;
-  const idUSER = await idTelegran.findAll({ attributes: { exclude: ['createdAt', 'updatedAt'] } })
+  time = `${now.getHours() + 2}:${now.getMinutes()}`;
+  const idUSER = await idTelegran.findAll({
+    attributes: { exclude: ['createdAt', 'updatedAt'] },
+  });
   if (id.length === 0) {
     await idUSER.map(x => id.push(x.dataValues.tel_id));
-  };
+  }
   console.log('------------------------------------');
   console.log(`brief information: `);
   console.log('------------------------------------');
@@ -37,23 +57,25 @@ async function testUserDB() {
   console.log('------------------------------------');
   console.log(`time Server: ${time}`);
   console.log('------------------------------------');
-  return
+  return {};
 }
 testUserDB();
 
-
-
-bot.onText(/\/start/, async (msg) => {
+bot.onText(/\/start/, async msg => {
   try {
-    bot.sendMessage(msg.chat.id, `Добрый день, ${msg.chat.first_name}. Добро пожаловать!`, {
+    bot.sendMessage(
+      msg.chat.id,
+      `Добрый день, ${msg.chat.first_name}. Добро пожаловать!`,
+      {},
+    );
+    const idUSER = await idTelegran.findAll({
+      attributes: { exclude: ['createdAt', 'updatedAt'] },
     });
-    const idUSER = await idTelegran.findAll({ attributes: { exclude: ['createdAt', 'updatedAt'] } })
     const usersAl2 = idUSER.map(x => x.dataValues.tel_id);
-    id = new Array;
+    id = [];
     if (id.length === 0) {
       await idUSER.map(x => id.push(x.dataValues.tel_id));
-    };
-
+    }
 
     testUserDB();
     const testUser = usersAl2.includes(msg.chat.id);
@@ -61,53 +83,50 @@ bot.onText(/\/start/, async (msg) => {
       console.log('вы подписаны на рассылку!');
       await idTelegran.create({
         tel_id: msg.chat.id,
-      })
+      });
       id.push(msg.chat.id);
-      bot.sendMessage(msg.chat.id, `${msg.chat.first_name}. вы подписаны на рассылку!`, {});
-      return
+      bot.sendMessage(
+        msg.chat.id,
+        `${msg.chat.first_name}. вы подписаны на рассылку!`,
+        {},
+      );
+      return;
     }
     console.log('вы УЖЕ подписаны на рассылку!');
-    bot.sendMessage(msg.chat.id, `${msg.chat.first_name}. вы уже подписаны на рассылку!`, {});
-    return
-
+    bot.sendMessage(
+      msg.chat.id,
+      `${msg.chat.first_name}. вы уже подписаны на рассылку!`,
+      {},
+    );
+    return;
   } catch (error) {
     console.log(error);
   }
-})
+});
 
-
-
-bot.onText(/\/weather/, async (msg) => {
-  let userId = msg.from.id;
+bot.onText(/\/weather/, async msg => {
+  const userId = msg.from.id;
   try {
-
     const city = 'cherkasy';
     const { apiKeyWeather } = process.env;
-    const units = 'metric'
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKeyWeather}&units=${units}`
+    const units = 'metric';
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKeyWeather}&units=${units}`;
 
     request(url, (err, response, body) => {
       if (err) {
-        console.log('error:', err)
+        console.log('error:', err);
       } else {
-        const weather = JSON.parse(body)
+        const weather = JSON.parse(body);
         const response1 = `*Сейчас: ${weather.main.temp} градусов в ${weather.name}*
         Влажность: ${weather.main.humidity} %
-        Облачность: ${weather.clouds.all} %`
+        Облачность: ${weather.clouds.all} %`;
         bot.sendMessage(userId, response1, { parse_mode: 'Markdown' });
       }
-    })
-
-
+    });
   } catch (error) {
     console.log(error);
   }
-})
-
-
-
-
-
+});
 
 setInterval(() => {
   const now = new Date();
@@ -116,106 +135,98 @@ setInterval(() => {
   if (now.getDay() >= 1 && now.getDay() <= 5) {
     if (time === '8:28') {
       for (let i = 0; i < id.length; i += 1)
-        bot.sendMessage(id[i], 'Хорошего рабочего дня!!!')
+        bot.sendMessage(id[i], 'Хорошего рабочего дня!!!');
     }
-
-
-
-
-
-
-
 
     if (time === '17:29') {
       for (let i = 0; i < id.length; i += 1) {
-
         const city = 'cherkasy';
         const { apiKeyWeather } = process.env;
-        const units = 'metric'
-        const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKeyWeather}&units=${units}`
+        const units = 'metric';
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKeyWeather}&units=${units}`;
 
         request(url, (err, response, body) => {
           if (err) {
-            console.log('error:', err)
+            console.log('error:', err);
           } else {
-            const weather = JSON.parse(body)
+            const weather = JSON.parse(body);
             const response1 = `*Сейчас: ${weather.main.temp} градусов в ${weather.name}*
             Влажность: ${weather.main.humidity} %
-            Облачность: ${weather.clouds.all} %`
+            Облачность: ${weather.clouds.all} %`;
             bot.sendMessage(id[i], response1, { parse_mode: 'Markdown' });
           }
-        })
-
+        });
       }
-
-
     }
-
-
-
 
     if (time === '10:28') {
       for (let i = 0; i < id.length; i += 1) {
-        bot.sendMessage(id[i], 'Скоро перерыв')
+        bot.sendMessage(id[i], 'Скоро перерыв');
       }
     }
 
     if (time === '10:39') {
       for (let i = 0; i < id.length; i += 1) {
-        bot.sendMessage(id[i], 'Перерыв окончен')
+        bot.sendMessage(id[i], 'Перерыв окончен');
       }
     }
 
     if (time === '12:25') {
       for (let i = 0; i < id.length; i += 1) {
-        bot.sendMessage(id[i], 'Обед через 5 мин.')
+        bot.sendMessage(id[i], 'Обед через 5 мин.');
       }
+    }
+
+    if (time === '12:35') {
+      text().then(x => {
+        for (let i = 0; i < id.length; i += 1) {
+          bot.sendMessage(id[i], `${x}`);
+        }
+      });
     }
 
     if (time === '12:59') {
       for (let i = 0; i < id.length; i += 1) {
-        bot.sendMessage(id[i], 'Уже нужно работать!!!')
+        bot.sendMessage(id[i], 'Уже нужно работать!!!');
       }
     }
 
     if (time === '14:28') {
       for (let i = 0; i < id.length; i += 1) {
-        bot.sendMessage(id[i], 'Скоро перерыв')
+        bot.sendMessage(id[i], 'Скоро перерыв');
       }
     }
 
     if (time === '14:39') {
       for (let i = 0; i < id.length; i += 1) {
-        bot.sendMessage(id[i], 'Перерыв окончен')
+        bot.sendMessage(id[i], 'Перерыв окончен');
       }
     }
 
     if (time === '15:58') {
       for (let i = 0; i < id.length; i += 1) {
-        bot.sendMessage(id[i], 'Скоро перерыв')
+        bot.sendMessage(id[i], 'Скоро перерыв');
       }
     }
 
     if (time === '16:10') {
       for (let i = 0; i < id.length; i += 1) {
-        bot.sendMessage(id[i], 'Перерыв окончен')
+        bot.sendMessage(id[i], 'Перерыв окончен');
       }
     }
-
 
     if (time === '17:30') {
       for (let i = 0; i < id.length; i += 1) {
-        bot.sendMessage(id[i], 'Пора домой!!! До завтра!')
+        bot.sendMessage(id[i], 'Пора домой!!! До завтра!');
       }
     }
   }
-
 }, 58000);
 
-setInterval(function () {
-  https.get("https://fabrnew.herokuapp.com/");
+setInterval(function() {
+  https.get('https://fabrnew.herokuapp.com/');
 }, 300000); // every 5 minutes (300000)
 
 app.listen(process.env.PORT || 4040, () => {
   console.log(`Server Work`);
-})
+});
