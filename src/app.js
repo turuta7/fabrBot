e64a909d117769fe5b983e6e6cfffa4cbee3c339
turuta7 <turuta51@gmail.com>
2019-11-07 11:40:54 +0200

    fixedProblem

--- src/app.js
+++ src/app.js
@@ -117,7 +117,7 @@ bot.onText(/\/start/, async msg => {
 // --------------------------------------------------
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-  if (msg.text.toString() === Погода) {
+  if (msg.text.toString() === 'Погода') {
     return request(url, (err, resp, body) => {
       bot.sendMessage(msg.chat.id, 'Ok');
     });
