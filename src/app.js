36ff46a660e959451e5a9035c9ebd333c918fd95
turuta7 <turuta51@gmail.com>
2019-11-07 17:31:14 +0200

    fixedOmage

--- src/app.js
+++ src/app.js
@@ -148,6 +148,14 @@ bot.onText(/\/photo/, async msg => {
     .on('error', (err) => {
       console.log(err)
     })
+    .pipe(fs.createWriteStream('photo.png'))
+  fs.readFile('photo.png', function (err, data) {
+    bot.sendPhoto({
+      chat_id: chatId,
+      caption: 'This is my test image',
+      photo: data//replace your image url here
+    })
+  })
     .on('response', (response) => {
 
       let url = response.request.href;
